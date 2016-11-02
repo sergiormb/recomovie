a271c353d7292e91ab55470093ca203deaf40e71
sergio <sergiormb88@gmail.com>
2016-11-02 19:26:02 +0100

    Cambiando la llamada a la función

--- recomovie/movies/filmaffinity.py
+++ recomovie/movies/filmaffinity.py
@@ -7,12 +7,12 @@ import requests
 
 def get_average_movie_filmaffinity(title):
     page_results = requests.get('http://www.filmaffinity.com/es/search.php?stext=' + title)
-    soup = BeautifulSoup.BeautifulSoup(page_results.content)
+    soup = BeautifulSoup(page_results.content)
     movie = soup.find("div", {"class": 'movie-card movie-card-1'})
     if movie:
         id_film = int(movie['data-movie-id'])
         page = requests.get('http://www.filmaffinity.com/es/film' + str(id_film) +'.html')
-        soup = BeautifulSoup.BeautifulSoup(page.content)
+        soup = BeautifulSoup(page.content)
         average = soup.find("div", {"id":'movie-rat-avg'})
         if average:
             return float(average['content'])
